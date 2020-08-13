078a4e68006b22e62ffe44c662dcc9f9bc7b7a88
Raghu Betina <raghu@firstdraft.com>
2020-08-13 11:03:54 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,16 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :username, :string
+  attribute :fname, :string
+  attribute :lname, :string
+  attribute :image, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
